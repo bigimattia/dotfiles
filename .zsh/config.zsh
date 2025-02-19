bcca94a0d448d849db6cfae4c020c841f2e0c926
bigimattia <mattia.bigi@outlook.it>
2025-02-19 12:13:07 +0100

    simplified zsh

---
+++ .zsh/config.zsh
@@ -0,0 +1,17 @@
+# If ZSH is not defined, use the current script's directory.
+[[ -n "$ZSH" ]] || export ZSH="${${(%):-%x}:a:h}"
+
+## History file configuration
+[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
+[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
+[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
+## History command configuration
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
+
+#source plugins
+source $ZSH/plugins.zsh
