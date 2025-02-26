d9f5376df6a9ee9ff2dabae6f1f121e00c620fe7
bigimattia <mattia.bigi@outlook.it>
2025-02-26 17:24:56 +0100

    edited hypr config

--- scripts/arch/extra/base.sh
+++ scripts/arch/extra/base.sh
@@ -2,6 +2,9 @@
 source scripts/arch/util.sh
 
 basePackages=(
+  networkmanager # already contains nmtui command
+  nm-connection-editor
+  network-manager-applet #
   #libsecret
   #gnome-keyring
   flatpak
@@ -14,6 +17,10 @@ printf "Installing base Packages...\n"
 for BASE_PACKAGES in "${basePackages[@]}"; do
     install_package "$BASE_PACKAGES"
 done
+
+#enables NetworkManager for handling power profiles in an automated fashion
+printf " Activating NetworkManager Services...\n"
+sudo systemctl enable --now NetworkManager
 
 # Add user to the gamemode group
 printf " Adding user to the gamemode group...\n"
