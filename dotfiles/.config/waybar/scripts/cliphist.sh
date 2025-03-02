18928c1d7c6b0d9d01380623215f16b1bf1d5c92
bigimattia <mattia.bigi@outlook.it>
2025-03-02 17:35:47 +0100

    cliphist fixes

--- dotfiles/.config/waybar/scripts/cliphist.sh
+++ dotfiles/.config/waybar/scripts/cliphist.sh
@@ -2,16 +2,16 @@
 
 case $1 in
     d)
-        cliphist list | rofi -dmenu -replace -config ~/.config/rofi/config-cliphist.rasi | cliphist delete
+        cliphist list | wofi -dmenu | cliphist delete
         ;;
 
     w)
-        if [ $(echo -e "Clear\nCancel" | rofi -dmenu -config ~/.config/rofi/config-short.rasi) == "Clear" ]; then
+        if [ $(echo -e "Clear\nCancel" | wofi -dmenu) == "Clear" ]; then
             cliphist wipe
         fi
         ;;
 
     *)
-        cliphist list | rofi -dmenu -replace -config ~/.config/rofi/config-cliphist.rasi | cliphist decode | wl-copy
+        cliphist list | wofi -dmenu | cliphist decode | wl-copy
         ;;
 esac
