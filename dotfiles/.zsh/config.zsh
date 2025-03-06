52fd1cb5907f8c55b487be1c4fd41cde59a3bf81
bigimattia <mattia.bigi@outlook.it>
2025-03-06 18:29:16 +0100

    zsh tab autocomplete

--- dotfiles/.zsh/config.zsh
+++ dotfiles/.zsh/config.zsh
@@ -1,3 +1,8 @@
+# Command completion with arrow-key
+autoload -Uz compinit
+compinit
+zstyle ':completion:*' menu select
+
 # If ZSH is not defined, use the current script's directory.
 [[ -n "$ZSH" ]] || export ZSH="${${(%):-%x}:a:h}"
 
