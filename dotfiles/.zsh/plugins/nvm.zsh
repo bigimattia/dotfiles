9d20828d0f61f60c4d046caecbc14015824001f1
Mattia <mattia.bigi@outlook.it>
2025-11-02 19:21:29 +0100

    less intrusive nvm autoload

--- dotfiles/.zsh/plugins/nvm.zsh
+++ dotfiles/.zsh/plugins/nvm.zsh
@@ -4,10 +4,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm_auto_use() {
-  local node_version="$(nvm version)"
   local nvmrc_path="$(nvm_find_nvmrc)"
-
+  
   if [ -n "$nvmrc_path" ]; then
+    local node_version="$(nvm version)"
     local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
 
     if [ "$nvmrc_node_version" = "N/A" ]; then
@@ -15,9 +15,6 @@ nvm_auto_use() {
     elif [ "$nvmrc_node_version" != "$node_version" ]; then
       nvm use
     fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
   fi
 }
 add-zsh-hook chpwd nvm_auto_use
