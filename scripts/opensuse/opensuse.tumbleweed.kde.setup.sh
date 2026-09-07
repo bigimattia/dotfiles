d9429bb3ac3db86ef01c75dc6634fdb10d43c2f0
Mattia <mattia.bigi@outlook.it>
2026-09-07 11:29:25 +0200

    Update opensuse.tumbleweed.kde.setup.sh

--- scripts/opensuse/opensuse.tumbleweed.kde.setup.sh
+++ scripts/opensuse/opensuse.tumbleweed.kde.setup.sh
@@ -10,12 +10,12 @@ fi
 
 #TODO : improve detection / handling of packages
 
+# Lock patterns
+sudo zypper al -t pattern kde_yast patterns-base-x11 plasma6-session-x11 yast2_basis yast2_basis yast2_desktop yast2_server x11_yast x11 technical_writing sw_management office multimedia kde_pim kde imaging games documentation books
+sudo zypper al patterns-base-x11 plasma6-session-x11 MozzilaFirefox
+
 # Install patterns
 sudo zypper install -t pattern base enhanced_base kde_plasma selinux
-
-# Lock patterns
-sudo zypper al -t pattern kde_yast patterns-base-x11 plasma6-session-x11 yast2_basis
-sudo zypper al patterns-base-x11 plasma6-session-x11
 
 # Install extra packages
 sudo zypper install ark btrfsprogs chrony cryptsetup device-mapper discover6 \
@@ -23,19 +23,7 @@ sudo zypper install ark btrfsprogs chrony cryptsetup device-mapper discover6 \
     kernel-default kexec-tools myrlyn NetworkManager numactl nvme-cli \
     partitionmanager plasma-framework-lang plasma6-workspace-lang rpm sdbootutil \
     selinux-policy-targeted-gaming shim snapper spectacle systemd-boot \
-    systemsettings6-lang zsh tar
-
-# Configure SDDM for Wayland on openSUSE Tumbleweed KDE
-
-# Create/edit SDDM Wayland configuration
-sudo tee /etc/sddm.conf.d/10-wayland.conf > /dev/null <<EOF
-[General]
-DisplayServer=wayland
-GreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell
-
-[Wayland]
-CompositorCommand=kwin_wayland --drm --no-lockscreen
-EOF
+    systemsettings6-lang zsh tar sddm-config-wayland
 
 # Enable SDDM service (use force to override the legacy one)
 sudo systemctl enable --force sddm
